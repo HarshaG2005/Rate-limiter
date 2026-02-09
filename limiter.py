a0d922136b171e6b7c634658e79ec5bbc645dac5
Harsha Gayantha <harshaa7654@gmail.com>
2026-02-09 15:52:49 +0530

    Builted basic rate limiter with FixedWindow algorithm

---
+++ limiter.py
@@ -0,0 +1,30 @@
+from time import time,sleep
+class FixedWindow:
+    def __init__(self,capacity,forward_callback,backward_callback):
+        self.capacity=capacity
+        self.allowance=capacity
+        self.forward_callback=forward_callback
+        self.backward_callback=backward_callback
+        self.current_time=int(time())
+
+    def handle(self,packet):
+        if int(time()) != self.current_time:
+            self.current_time=int(time())
+            self.allowance=self.capacity
+        if self.allowance < 1 :
+           return self.backward_callback(packet)
+            
+        self.allowance -= 1
+        return self.forward_callback(packet)
+        
+def forward(packet):
+    print(f'forward packet:{packet}')
+def drop(packet):
+    print(f'drop packet:{packet}')
+x=FixedWindow(1,forward,drop)
+packet=0
+while packet<10:
+    sleep(0.2)
+    x.handle(packet)
+    packet+=1
+      
