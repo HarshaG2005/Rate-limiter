3168ffb10981dacbef026d7252552fb32e323821
Harsha Gayantha <harshaa7654@gmail.com>
2026-02-11 12:50:52 +0530

    building foundation for a redis counter

---
+++ redis_2.py
@@ -0,0 +1,12 @@
+import redis 
+r=redis.Redis(host='localhost',port=6379,decode_responses=True)
+#Micro-lab B: Counter (rate limiting foundation)
+#DEL/delete=Removes a key (and its value) from Redis.
+r.delete('hits')
+print(r.incr('hits'))
+#incr=Increments the value stored at key by 1, but only if it’s an integer (or doesn’t exist)
+print(r.incr('hits'))
+print(r.incr('hits',10))
+#DECR DECREASE
+print(r.decr('hits',5))
+print(r.get('hits'))
